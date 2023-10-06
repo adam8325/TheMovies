6035084750c2a867801dd8b0b4eaf18785210e4f
Adam Eleraky <adam_eleraky@hotmail.com>
2023-10-06 09:53:31 +0200

    Add project files.

---
+++ Model/DataAccessHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Configuration;
+
+public class DataAccesHelper
+{
+
+    public static string CNNString(string name)
+    {
+        return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+    }
+}
---
+++ Model/Movies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Movies
+{
+	public int Id;
+	public string Title;
+	public int Length;
+	public string Genre;
+
+
+	public Movies(int id, string title, int length, string genre)
+	{
+		Id = id;
+		Title = title;
+		Length = length;
+		Genre = genre;
+	}
+	
+}
